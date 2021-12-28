a2e8186ee99dd1af11d42b49d01761a9b6ebbbb3
michael-pessoni <pessoni94@gmail.com>
2021-12-28 15:41:20 -0300

    Schedule Background Work

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -25,6 +25,11 @@ import com.example.android.devbyteviewer.repository.VideosRepository
 import retrofit2.HttpException
 
 class RefreshDataWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+
+    companion object{
+       const val WORK_NAME = "RefreshDataWorker"
+    }
+
     override suspend fun doWork(): Result {
         val database = getDatabase(applicationContext)
         val repository = VideosRepository(database)
